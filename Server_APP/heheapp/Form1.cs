a2d5a9a61e51e594db3b8dcdf7724bf04920ba2f
BUKC <umertanveer2002@gmail.com>
2024-12-12 13:53:18 +0500

    'hello'

---
+++ Server_APP/heheapp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace heheapp
+{
+    public partial class Form1 : Form
+    {
+       
+        TcpClient client= null;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  TcpClient client = null;
+            NetworkStream ns = client.GetStream();
+            StreamWriter writer = new StreamWriter(ns);
+            writer.WriteLine(textBox2.Text);
+            writer.Flush();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            client = new TcpClient("127.0.0.1", 8888);
+            NetworkStream ns=client.GetStream();
+            StreamReader sr=new StreamReader(ns);
+           // StreamWriter sw = new StreamWriter(ns);
+            textBox1.Text= sr.ReadLine();
+
+
+
+        }
+    }
+
+}
---
+++ Server_APP/server_hehe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace server_hehe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(8888);
+            server.Start();
+            Socket clientDoor=server.AcceptSocket();
+            if (clientDoor.Connected ) {
+                NetworkStream ns = new NetworkStream(clientDoor);
+                Console.WriteLine("Server >> Hello Client!!");
+                StreamWriter writer = new StreamWriter(ns);
+                writer.WriteLine("server >>Hello Solax, You are connected!!");
+                writer.Flush();
+                while (true)
+                {
+                    StreamReader sr= new StreamReader(ns);
+                    Console.WriteLine("Client>> "+sr.ReadLine());
+                }
+            }
+            server.Stop();
+
+        }
+    }
+}
